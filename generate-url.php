05599043cd633fc789a592689b3363de43a1c8d1
isha dalvi <dipeshxyzyadav@gmail.com>
2025-06-01 13:00:52 +0530

    Update generate-url.php

--- generate-url.php
+++ generate-url.php
@@ -2,7 +2,7 @@
 // API key aur destination URL define kar lete hain
 $apiKey = '2f15491d0c2b98eddcd7d9b32957df6088f00f90';
 $destinationUrl = 'https://pwthor.site/login-success.html';
-$alias = 'pw_thor'; // Optional alias
+$alias = 'pw_thor1'; // Optional alias
 
 // API URL ko prepare karte hain
 $apiUrl = "https://easysky.in/api?api=$apiKey&url=" . urlencode($destinationUrl) . "&alias=$alias&format=json";
