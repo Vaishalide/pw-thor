98024b88727b244b3822ca0f1267bd815454f2b2
isha dalvi <dipeshxyzyadav@gmail.com>
2025-06-01 13:25:55 +0530

    Update generate-url.php

--- generate-url.php
+++ generate-url.php
@@ -1,11 +1,11 @@
 <?php
 // API key aur destination URL define kar lete hain
-$apiKey = 'be1be8f8f3c02db2e943cc7199c5641971d86283';
+$apiKey = '2f15491d0c2b98eddcd7d9b32957df6088f00f90';
 $destinationUrl = 'https://pwthor.site/login-success.html';
 $alias = 'pw_thor'; // Optional alias
 
 // API URL ko prepare karte hain
-$apiUrl = "https://api.gplinks.com/api?api=$apiKey&url=" . urlencode($destinationUrl) . "&alias=$alias&format=json";
+$apiUrl = "https://easysky.in/api?api=$apiKey&url=" . urlencode($destinationUrl) . "&alias=$alias&format=json";
 
 // cURL se API call karte hain
 $ch = curl_init();
