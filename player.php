35269a22109f49b0d402d00df43990e0f63f0bed
isha dalvi <dipeshxyzyadav@gmail.com>
2025-06-05 14:21:46 +0530

    Update player.php

--- player.php
+++ player.php
@@ -134,10 +134,10 @@ $escapedTitle    = htmlspecialchars($title, ENT_QUOTES, 'UTF-8');
         window.hls = new Hls({ capLevelToPlayerSize:true });
         window.hls.loadSource(url);
         window.hls.attachMedia(video);
-        window.hls.on(Hls.Events.MANIFEST_PARSED, () => { video.currentTime = 0; video.play(); });
+        window.hls.on(Hls.Events.MANIFEST_PARSED, () => { video.currentTime = 7; video.play(); });
       } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
         video.src = url;
-        video.addEventListener('loadedmetadata', () => { video.currentTime = 0; video.play(); });
+        video.addEventListener('loadedmetadata', () => { video.currentTime = 7; video.play(); });
       }
     }
 
